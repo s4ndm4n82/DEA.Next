5dbf25f255091de47866bf97e4d45c143c224aa3
Sandaruwan Samaraweera <sandaruwan12@gmail.com>
2023-07-31 16:08:47 +0530

    Minor adjustments.

--- DEACleaner/Program.cs
+++ DEACleaner/Program.cs
@@ -1,9 +1,20 @@
-﻿using LogFileCleaner;
+﻿using AppConfigReader;
+using LogFileCleaner;
 using WriteLog;
 
-int deleteStatus = LogFileCleanerClass.StartCleaner();
+AppConfigReaderClass.AppSettingsRoot jsonData = AppConfigReaderClass.ReadAppDotConfig();
+AppConfigReaderClass.Programsettings programSettings = jsonData.ProgramSettings;
 
-string logEntry = deleteStatus == 1 ? "Log file deletion ended successfully ...." : "log file deletion unsuccessfull ....";
-int logType = deleteStatus != 1 ? 0 : 1;
+if (programSettings.CleanLogs)
+{
+    int deleteStatus = LogFileCleanerClass.StartCleaner();
 
-WriteLogClass.WriteToLog(logType, logEntry, 1);
+    string logEntry = deleteStatus == 1 ? "Log file deletion ended successfully ...." : "log file deletion unsuccessfull ....";
+    int logType = deleteStatus != 1 ? 0 : 1;
+
+    WriteLogClass.WriteToLog(logType, logEntry, 1);
+}
+else
+{
+    WriteLogClass.WriteToLog(1, "Auto log cleaning is disabled. Please set to true in the config file or run manually.", 1);
+}
--- DEAMailer/Program.cs
+++ DEAMailer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AppConfigReader;
 using CreatEmail;
 using ErrorFolderChecker;
+using WriteLog;
 
 AppConfigReaderClass.AppSettingsRoot jsonData = AppConfigReaderClass.ReadAppDotConfig();
 AppConfigReaderClass.Programsettings programSettings = jsonData.ProgramSettings;
@@ -8,4 +9,8 @@ AppConfigReaderClass.Programsettings programSettings = jsonData.ProgramSettings;
 if (programSettings.SendErrorEmail)
 {
     CreateEmailClass.StartCreatingEmail(ErrorFolderCheckerClass.ErrorFolderChecker().Item2, ErrorFolderCheckerClass.ErrorFolderChecker().Item1);
+}
+else
+{
+    WriteLogClass.WriteToLog(1, "Auto error folder check and email sender is disabled. Please set to true in the config file or run manually.", 1);
 }
