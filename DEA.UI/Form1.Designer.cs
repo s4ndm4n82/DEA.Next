86fe4b0505dc83974856e03314e796bfe23a67dd
Sandaruwan Samaraweera <sadaruwan12@gmail.com>
2025-01-20 16:32:45 +0530

    Started creating a UI. Using WinForms.

---
+++ DEA.UI/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DEA.UI;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        SuspendLayout();
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.Color.Gray;
+        ClientSize = new System.Drawing.Size(806, 474);
+        ForeColor = System.Drawing.SystemColors.ControlLightLight;
+        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+        Text = "Main Window";
+        ResumeLayout(false);
+    }
+
+    #endregion
+}
