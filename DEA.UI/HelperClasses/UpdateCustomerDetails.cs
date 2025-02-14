8e09eb3ccbf124b2cae0dae364cd7104a571f273
Sandaruwan Samaraweera <sadaruwan12@gmail.com>
2025-02-14 16:30:50 +0530

    Fixed the edit function error.

--- DEA.UI/HelperClasses/UpdateCustomerDetails.cs
+++ DEA.UI/HelperClasses/UpdateCustomerDetails.cs
@@ -1,5 +1,6 @@
 ﻿using DEA.Next.Data;
 using DEA.Next.Entities;
+using DEA.Next.HelperClasses.OtherFunctions;
 using DEA.UI.Forms;
 
 namespace DEA.UI.HelperClasses
@@ -15,10 +16,56 @@ namespace DEA.UI.HelperClasses
                 if (customer != null)
                 {
                     // Update the existing customer details directly
+                    customer.Status = form.cusOnEdFrm.Checked;
                     customer.CustomerName = form.cusNameEdFrmTxt.Text;
-                    customer.UserName = form.cusUnameEdFrmTxt;
-                    customer.Token = form.cusApiTokenEdFrmTxt;
-                    customer.Queue = form.cusQueuEdFrmTxt;
+                    customer.UserName = form.cusUnameEdFrmTxt.Text;
+                    customer.Token = form.cusApiTokenEdFrmTxt.Text;
+                    customer.Queue = int.Parse(form.cusQueuEdFrmTxt.Text);
+                    customer.ProjectId = form.cusProjIdEdFrmTxt.Text;
+                    customer.TemplateKey = form.cusApiTokenEdFrmTxt.Text;
+                    customer.DocumentId = form.cusDocIdEdFrmTxt.Text;
+                    customer.DocumentEncoding = form.cusDocencEdFrmTxt.Text;
+                    customer.MaxBatchSize = int.Parse(form.cusMaxBatchEdFrmTxt.Text);
+                    customer.FieldOneName = form.cusFonNameEdFrmTxt.Text;
+                    customer.FieldOneValue = form.cusFonValEdFrmTxt.Text;
+                    customer.FieldTwoName = form.custFtwoNameEdFrmTxt.Text;
+                    customer.FieldTwoValue = form.custFtwoValEdFrmTxt.Text;
+                    customer.Domain = form.cusDomainEdFrmTxt.Text;
+                    customer.FileDeliveryMethod = form.cusDelMethodEdFrmCombo.SelectedItem?.ToString() ?? string.Empty;
+                    customer.DocumentDetails = form.cusDocExtListEdFrm.CheckedItems.Cast<string>().Select(ext => new DocumentDetails { Extension = ext }).ToList();
+                    customer.ModifiedDate = DateTime.Now;
+
+                    switch (customer.FileDeliveryMethod.ToLower())
+                    {
+                        case MagicWords.Ftp:
+                            // Update the existing FTP details directly
+                            if (customer.FtpDetails != null)
+                            {
+                                customer.FtpDetails.FtpType = form.ftpTypComboEdFrm.SelectedItem?.ToString()?.ToUpper() ?? "FTP";
+                                customer.FtpDetails.FtpProfile = form.ftpProfileComboEdFrm.SelectedItem?.ToString() ?? "profileepe";
+                                customer.FtpDetails.FtpHost = form.ftpHostEdFrmTxt.Text;
+                                customer.FtpDetails.FtpUser = form.ftpUserNameEdFrmTxt.Text;
+                                customer.FtpDetails.FtpPassword = form.ftpPasswordEdFrmTxt.Text;
+                                customer.FtpDetails.FtpPort = int.Parse(string.IsNullOrEmpty(form.ftpPortEdFrmTxt.Text) ? "21" : form.ftpPortEdFrmTxt.Text);
+                                customer.FtpDetails.FtpFolderLoop = form.ftpLoopOnEdFrm.Checked;
+                                customer.FtpDetails.FtpMoveToSubFolder = form.ftpMoveToSubOnEdFrm.Checked;
+                                customer.FtpDetails.FtpMainFolder = form.ftpMainPathEdFrmTxt.Text.Trim();
+                                customer.FtpDetails.FtpSubFolder = form.ftpSubPathEdFrmTxt.Text.Trim();
+                                customer.FtpDetails.FtpRemoveFiles = form.ftpRemoveOnEdFrm.Checked;
+                            }
+                            break;
+                        case MagicWords.Email:
+                            // Update the existing email details directly
+                            if (customer.EmailDetails != null)
+                            {
+                                customer.EmailDetails.Email = form.emlAddressEdFrmTxt.Text.Trim();
+                                customer.EmailDetails.EmailInboxPath = form.emlInboxPathEdFrmTxt.Text.Trim();
+                                customer.EmailDetails.SendSubject = form.emlSndSubjectOnEdFrm.Checked;
+                                customer.EmailDetails.SendEmail = form.emlSenAdressOnEdFrm.Checked;
+                                customer.EmailDetails.SendBody = form.emlSndBodyOnEdFrm.Checked;
+                            }
+                            break;
+                    }
                 }
 
                 // Save the changes
