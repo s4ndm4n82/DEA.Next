1f860ac6ce912ae7a1c52c80312a8c32804375c9
Sandaruwan Samaraweera <sadaruwan12@gmail.com>
2025-02-20 15:46:15 +0530

    Finished and released DEA.UI v1.0.0.14 (Sync).

--- DEA.UI/Versioning/AssemblyInfo.cs
+++ DEA.UI/Versioning/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 [assembly: AssemblyTitle("DEA.UI")]
 
 // Version information for an assembly consists of the following four values:
-[assembly: AssemblyVersion("1.0.0.15")]
-[assembly: AssemblyFileVersion("1.0.0.15")]
-[assembly: AssemblyInformationalVersion("1.0.0.15")]
+[assembly: AssemblyVersion("1.0.0.16")]
+[assembly: AssemblyFileVersion("1.0.0.16")]
+[assembly: AssemblyInformationalVersion("1.0.0.16")]
